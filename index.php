7b9483d03de32c0f5cd5097d8fe864bef99d6d36
KUNKUN2121 <kunkun7532@gmail.com>
2022-02-08 22:22:51 +0900

    permalink

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
             while ( have_posts() ) : the_post(); ?>
                   <div class="naka">
                     <!-- カテゴリー表示 --> <?php $this_categories = get_the_category();if ( $this_categories ) {$this_category_color = get_field( 'color', 'category_' . $this_categories[0]->term_id );$this_category_name  = $this_categories[0]->name; echo '<span class="entry-label" style="' . esc_attr( 'background:' . $this_category_color ) . ';">' . esc_html( $this_category_name ) . '</span>';}?>
-                    <a href="aa" id="title"><?php the_title(); ?></a>  <!-- タイトル表示 -->
+                    <a href="<?php the_permalink(); ?>" id="title"><?php the_title(); ?></a>  <!-- タイトル表示 -->
                     <a href="aa" id="title2">タップで詳細表示</a>
                     <p>
                     <spawn>投稿日: <?php the_time('n月j日'); ?> @ <?php the_time('G:i'); ?> 

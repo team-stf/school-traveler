07704b63c6d7e9a6056df2addc2686bd115c16db
KUNKUN2121 <kunkun7532@gmail.com>
2022-02-09 11:23:16 +0900

    404 and おしらせ追加

---
+++ 404.php
@@ -0,0 +1,19 @@
+<!-- 404.php -->
+<?php get_header(); ?>
+<style>
+    .error-404{
+        padding:10px 30px;
+        font-size: 20px;
+    }
+    a{
+        color:black;
+    }
+</style>
+<div class="error-404">
+    <img class="none" src="<?php echo get_template_directory_uri(); ?>/images/w-amidc.png" width="100%" height="100%"></var>
+    <h1>404 Not Found（ページが見つかりませんでした）</h1>
+    <p>指定された以下のページは存在しないか、または移動した可能性があります。</p>
+    <p class="error_url"><span><?php echo get_pagenum_link(); ?></span></p>
+    <br>
+  <h1><a href="<?php echo home_url(); ?>"> ホームに戻る</a></h1>
+</div>

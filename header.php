9fa0779db3448f67f1816ed0c5d141609a648b92
matsudakatio <77916609+matsudakatio@users.noreply.github.com>
2022-02-10 11:32:16 +0900

    Revert "メインビジュ挿入"

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
    <meta charset="utf-8">
    <meta name="robots" content="noindex,nofollow">
-   <title>TEST</title>
+   <title>OKINAWATEST1</title>
 
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 

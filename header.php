aed381d366bf36e6cd9d9e439240d76adc72df9a
matsudakatio <77916609+matsudakatio@users.noreply.github.com>
2022-02-10 23:42:28 +0900

    フッター改善

--- header.php
+++ header.php
@@ -82,20 +82,15 @@
 });
    </script>
 
-   <!-- <script>
+   <script>
 $(function () {
   var webStorage = function () {
     if (sessionStorage.getItem('access')) {
       /*
         2回目以降アクセス時の処理
       */
-      $(".loader-bg").addClass('is-active');
+      $("#loader-bg").addClass('is-active');
     } else {
-      /*
-        初回アクセス時の処理
-      */
-      sessionStorage.setItem('access', 'true'); // sessionStorageにデータを保存
-      $(".loading-animation").addClass('is-active'); // loadingアニメーションを表示
       $(window).on('load',function(){
  $("#loader-bg").delay(1000).fadeOut('slow');
  //ローディング画面を3秒（3000ms）待機してからフェードアウト
@@ -104,7 +99,7 @@ $(function () {
   }
   webStorage();
 });
-   </script> -->
+   </script>
 
 
 <!-- headue -->

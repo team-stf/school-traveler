cd5f907df50f023c5ee7e72b6cdadde5d985b347
matsudakatio <77916609+matsudakatio@users.noreply.github.com>
2022-03-02 22:27:19 +0900

    部屋割り変更1

--- index.php
+++ index.php
@@ -363,7 +363,7 @@ $(window).on('load', function(){
                     <article>
                       <div class="events">
                           <div class="event">
-                            <p class="time">9:??</p>
+                            <p class="time">08:20</p>
                             <p class="name">集合: 名古屋駅</p>
                           </div>
                         
@@ -624,21 +624,10 @@ $(window).on('load', function(){
           <option value="room/g2b">G2B</option>
           <option value="room/g2c">G2C</option>
           <option value="room/g2d">G2D</option>
-          <option value="room/g2e">G2E</option>
-          <option value="room/g2f">G2F</option>
-          <option value="room/g2g">G2G</option>
           <option value="room/g2h">G2H</option>
           <option value="room/g2i">G2I</option>
           <option value="room/g2j">G2J</option>
           <option value="room/g2k">G2K</option>
-      </optgroup>
-      <optgroup label="専門科">
-        <option value="room/t2a">T2A</option>
-        <option value="room/t2b">T2B</option>
-        <option value="room/t2c">T2C</option>
-        <option value="room/t2d">T2D</option>
-        <option value="room/t2e">T2E</option>
-        <option value="room/j2a">J2A</option>
       </optgroup>
   </select>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
